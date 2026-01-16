e849bfd5725759cf800545267c627778c5724a10
이유나 <yn151325@gmail.com>
2026-01-16 14:52:50 +0900

    Optimize string concatenation in 세로 읽기.java
    Refactor string concatenation to use StringBuilder for efficiency.

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -15,7 +15,10 @@ class Solution {
         }
         
         for (int i=0; i<n; i++) {
-            answer += arr[i][c-1];   
+            answer += arr[i][c-1];
+            // java에서 + 연산자는 한 쪽이라도 String 타입이면 나머지도 String 타입으로 자동 형변환
+            // 내부적으로 실제 일어나는 일: answer = new StringBuilder(answer).append(arr[i][c-1]).toString();
+            // 설명: 기존 String을 기반으로 StringBuilder 생성 → append 수행 → 다시 String으로 변환
         }
         return answer;
     }
