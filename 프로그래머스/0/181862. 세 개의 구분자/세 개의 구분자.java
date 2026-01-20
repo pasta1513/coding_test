c697422cb7da185fd7d64107f724426f3a536c3f
이유나 <yn151325@gmail.com>
2026-01-20 12:02:36 +0900

    Enhance comments in the solution method
    Add comments explaining the split method usage in solution.

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -3,6 +3,8 @@ import java.util.*;
 class Solution {
     public String[] solution(String myStr) {
         ArrayList<String> list = new ArrayList<String>();
+        // 대괄호 []는 안에 있는 문자 중 하나라는 뜻
+        // 즉 [abc] -> a 또는 b 또는 c
         String[] myStr_split = myStr.split("[abc]");
         
         for (int i=0; i<myStr_split.length; i++) {
