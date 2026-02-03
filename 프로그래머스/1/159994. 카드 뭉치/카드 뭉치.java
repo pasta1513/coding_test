a9b7088869ea6041f1006aab92432db67e4d1dbe
이유나 <yn151325@gmail.com>
2026-02-03 17:43:14 +0900

    Simplify index tracking in solution method
    Refactored solution method to simplify index tracking.

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,30 +1,16 @@
-import java.util.HashMap;
-
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        HashMap<String, Integer> cards1_map = new HashMap<String, Integer>();
-        HashMap<String, Integer> cards2_map = new HashMap<String, Integer>();
-        
-        for (int i=0; i<cards1.length; i++) {
-            cards1_map.put(cards1[i], i);
-        }
-        for (int i=0; i<cards2.length; i++) {
-            cards2_map.put(cards2[i], i);
-        }
-        
-        int cards1_idx = 0;
-        int cards2_idx = 0;
-        
+        int i = 0, j = 0;
+
         for (String word : goal) {
-            if (cards1_map.containsKey(word) && cards1_map.get(word)==cards1_idx) {
-                cards1_idx++;
-            } else if (cards2_map.containsKey(word) && cards2_map.get(word)==cards2_idx) {
-                cards2_idx++;
+            if (i < cards1.length && cards1[i].equals(word)) {
+                i++;
+            } else if (j < cards2.length && cards2[j].equals(word)) {
+                j++;
             } else {
                 return "No";
             }
         }
-        
         return "Yes";
     }
 }
