bd62759cc46d2140bbb92292536f884d6b65cd6b
이유나 <yn151325@gmail.com>
2026-02-06 14:08:38 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.25 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,20 +1,13 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int ldx=wallpaper.length;
-        int ldy=wallpaper[0].length();
+        int ldx=wallpaper.length, ldy=wallpaper[0].length();
+        int rdx = 0, rdy = 0;
+        
         for (int i=0; i<wallpaper.length; i++) {
             for (int j=0; j<wallpaper[0].length(); j++) {
                 if(wallpaper[i].charAt(j) == '#') {
                     ldx = Math.min(ldx, i);
                     ldy = Math.min(ldy, j);
-                }
-            }
-        }
-        
-        int rdx = 0, rdy = 0;
-        for (int i=0; i<wallpaper.length; i++) {
-            for (int j=0; j<wallpaper[0].length(); j++) {
-                if(wallpaper[i].charAt(j) == '#') {
                     rdx = Math.max(rdx, i+1);
                     rdy = Math.max(rdy, j+1);
                 }
