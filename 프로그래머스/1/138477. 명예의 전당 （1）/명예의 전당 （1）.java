2237932bca385fe7119d199aef37a494aac97eb8
이유나 <yn151325@gmail.com>
2026-02-20 16:24:45 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.87 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        int[] answer = new int[score.length];
+        
+        for (int i=0; i<score.length; i++) {
+            pq.add(score[i]);
+            
+            if (pq.size() > k) {
+                pq.poll();
+            }
+            
+            answer[i] = pq.peek();
+        }
+        
+        return answer;
+    }
+}
