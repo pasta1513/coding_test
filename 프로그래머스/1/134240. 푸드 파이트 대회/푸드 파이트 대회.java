3c11498cd5a126cbb4ab4ae7fdf0644887a72dbb
이유나 <yn151325@gmail.com>
2026-02-20 14:50:18 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 20.32 ms, Memory: 96.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(int[] food) {
+        int[] num = new int[food.length];
+        
+        for (int i=1; i<food.length; i++) {
+            num[i] = food[i]/2;
+        }
+        
+        String answer = "";
+        
+        for (int i=0; i<num.length; i++) {
+            if (num[i] == 0) continue;
+            
+            for (int j=0; j<num[i]; j++) {
+                answer += String.valueOf(i);
+            }
+        }
+        
+        answer += "0";
+        
+        for (int i=num.length-1; i>=0; i--) {
+            if (num[i] == 0) continue;
+            
+            for (int j=0; j<num[i]; j++) {
+                answer += String.valueOf(i);
+            }
+        }
+        
+        return answer;
+    }
+}
