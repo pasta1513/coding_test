42158b65d9d50d3cb55ecb3dfd1cc104c0300a21
이유나 <yn151325@gmail.com>
2026-03-13 00:20:38 +0900

    [level 1] Title: 문자열 나누기, Time: 1.03 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int same = 0;
+        int different = 0;
+        char x = s.charAt(0);
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (same == 0 && different == 0) {
+                x = s.charAt(i);
+            }
+
+            if (s.charAt(i) == x) same++;
+            else different++;
+
+            if (same == different) {
+                answer++;
+                same = 0;
+                different = 0;
+            }
+        }
+
+        if (same != 0 || different != 0) answer++;
+        
+        return answer;
+    }
+}
