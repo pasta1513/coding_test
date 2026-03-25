d0ac6bb46fde7d979a27ba1395bfa39dbc59fe96
이유나 <yn151325@gmail.com>
2026-03-25 23:23:25 +0900

    Refactor bitwise operation and variable naming

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도(Refactoring).java
@@ -0,0 +1,21 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            int merged = arr1[i] | arr2[i];            
+            String binary = Integer.toBinaryString(merged);
+
+            while (binary.length() < n) {
+                binary = "0" + binary;
+            }
+
+            binary = binary.replace('1', '#');
+            binary = binary.replace('0', ' ');
+
+            answer[i] = binary;
+        }
+
+        return answer;
+    }
+}
