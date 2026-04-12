658c4c67055f8904df85cf75391844349085dc70
이유나 <yn151325@gmail.com>
2026-04-13 00:05:18 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String phone_number) {
+        StringBuilder sb = new StringBuilder();
+        int n = phone_number.length();
+        
+        for (int i=0; i<n - 4; i++) {
+            sb.append("*");
+        }
+        
+        sb.append(phone_number.substring(n - 4, n));
+        
+        return sb.toString();
+    }
+}
