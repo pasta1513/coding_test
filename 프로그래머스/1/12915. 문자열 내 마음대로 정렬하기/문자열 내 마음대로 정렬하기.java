850f9e68355a08a2995430c14c2ce2ab48662d5e
이유나 <yn151325@gmail.com>
2026-04-14 23:33:20 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.60 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -8,10 +8,12 @@ class Solution {
             map.put(i, strings[i].substring(n, n+1));
         }
         
+        // 인덱스 n번째 글자 기준 오름차순 정렬
         List<Integer> keySet = new ArrayList<>(map.keySet());
         keySet.sort((o1, o2) -> {
             int cmp = map.get(o1).compareTo(map.get(o2));
             
+            // 같은 문자일 때는 문자열 전체로 비교
             if (cmp == 0) {
                 return strings[o1].compareTo(strings[o2]);
             }
